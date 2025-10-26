d4f9bd6270f0463155fd310ccab05d79f832e021
Megan Seaman <meganseaman@wifi-88-113.nwpc.iup.edu>
2025-10-26 14:47:51 -0400

    making sure everything is working

--- CMSC319Project.py
+++ CMSC319Project.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+clientID = os.getenv("CLIENT_ID")
+clientSecret = os.getenv("CLIENT_SECRET")
+
+print(clientID, clientSecret)
